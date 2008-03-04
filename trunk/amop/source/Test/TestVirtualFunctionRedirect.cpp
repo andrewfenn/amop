e42c2bafa1d9d3dc2b6e248f89f37990395851fe
rdescartes <rdescartes@0602518e-0d43-0410-b636-dd587e7f3de9>
2008-03-04 14:10:24 +0000

    Turn project warning level to 4. Disable some insane VC level 4 Warning . Make it more thread safe. (But not using static variable to check the offset.)

--- trunk/amop/source/Test/TestVirtualFunctionRedirect.cpp
+++ trunk/amop/source/Test/TestVirtualFunctionRedirect.cpp
@@ -21,12 +21,12 @@ public:
 //------------------------------------------------------------------
 TEST(TestGetOffset)
 {
-	CHECK_EQUAL(1, Detail::GetOffset(&IInterface::foo2));
-	CHECK_EQUAL(0, Detail::GetOffset(&IInterface::foo1));
+    CHECK_EQUAL(1, Detail::Inner::TCheckOffset::GetOffset(&IInterface::foo2));
+	CHECK_EQUAL(0, Detail::Inner::TCheckOffset::GetOffset(&IInterface::foo1));
 
-	CHECK_EQUAL(4, Detail::GetOffset(&IInterface::foo5));
-	CHECK_EQUAL(5, Detail::GetOffset(&IInterface::foo6));
-	CHECK_EQUAL(6, Detail::GetOffset(&IInterface::foo7));	
+	CHECK_EQUAL(4, Detail::Inner::TCheckOffset::GetOffset(&IInterface::foo5));
+	CHECK_EQUAL(5, Detail::Inner::TCheckOffset::GetOffset(&IInterface::foo6));
+	CHECK_EQUAL(6, Detail::Inner::TCheckOffset::GetOffset(&IInterface::foo7));	
 }
 
 //------------------------------------------------------------------
@@ -55,29 +55,3 @@ class D: public A, public B {
     virtual int Dfunc() { return 5; };
 };
 
-//------------------------------------------------------------------
-TEST(TestGetOffsetSame)
-{
-	Detail::TCheck* check = Detail::CreateCheckObject();
-	IInterface* inter = (IInterface*) check;
-	
-	CHECK_EQUAL(inter->foo1(), Detail::GetOffset(&IInterface::foo1));
-	CHECK_EQUAL(inter->foo6(), Detail::GetOffset(&IInterface::foo6));
-}
-
-//------------------------------------------------------------------
-TEST(TestGetOffsetNotPureVirtual)
-{
-	bool isThrow = false;
-	
-	try
-	{
-		Detail::GetOffset(&B::Bfunc);
-	}
-	catch(const TNotPureVirtualException& )
-	{
-		isThrow = true;	
-	}	
-
-	CHECK(isThrow);
-}
